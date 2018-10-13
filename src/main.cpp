60ce5d77f7ab4c7645977bb6f2c5cb51ac4b097b
Charlie Wang <charliew.26@gmail.com>
2018-10-12 23:52:41 -0700

    Added matrix class and niave matrix multplication algorithm.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <memory>
+#include <vector>
 #include <sys/time.h>
 
 uint64_t get_time()
@@ -9,11 +11,36 @@ uint64_t get_time()
 }
 
 #define N 3000
-double a[N][N];
-double b[N][N];
 
+template <class T>
+struct mat
+{
+  mat(size_t nrow, size_t ncol) : nrow_(nrow), ncol_(ncol)
+  {
+    vv = std::vector<std::vector<T>>(nrow_, std::vector<T>(ncol_, 0));
+  }
 
-int main(int argc, char * argv[])
+  std::vector<T>& operator[] (size_t i)
+  {
+    return vv[i];
+  }
+
+  size_t nrow() { return nrow_; }
+  size_t ncol() { return ncol_; }
+  
+  using matrix_t = std::vector<std::vector<T>>;
+  typename matrix_t::iterator begin() { return vv.begin(); }
+  typename matrix_t::iterator end()   { return vv.end();   }
+private:
+  matrix_t vv;
+  size_t nrow_;
+  size_t ncol_;
+};
+
+// shows the delta in performance when memory is written non-sequentially
+// (bad spatial locality)
+template <class T>
+void matrix_initialize_test(mat<T> &a, mat<T> &b)
 {
   std::cout << "begin allocation" << std::endl;
   
@@ -30,6 +57,41 @@ int main(int argc, char * argv[])
       b[j][i] = 0;
   t2 = get_time();
   std::cout << "Inner loop: row time = " << ((float)(t2-t1)) / 1000000.0 << std::endl;
-	
+
+}
+
+/*
+matrix multiplication tests
+ */
+template <class T>
+mat<T> mmult_bad(mat<T> a, mat<T> b)
+{
+  if (a.ncol() != b.nrow())
+    throw std::invalid_argument("mmult : invalid matrix dimensions.");
+
+  mat<T> c(a.nrow(), b.ncol());
+  for (size_t i = 0; i < a.nrow(); ++i)
+    for (size_t j = 0; j < b.ncol(); ++j)
+      for (size_t k = 0; k < a.ncol(); ++k)
+	c[i][j] += a[i][k] * b[k][j];
+
+  return c;
+}
+
+template <class T>
+void print_mat(mat<T> &m)
+{
+  for (const auto& row : m)
+  {
+    for (const auto& e : row)
+      std::cout << e << " ";
+    std::cout << std::endl;
+  }
+}
+
+int main(int argc, char * argv[])
+{
+  mat<double> m(N,N);
+  matrix_initialize_test(m, m);
   return 0;
 }
