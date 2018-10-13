e55448facca284ed8f23e152ec40269e69b56da8
Charlie Wang <charliew.26@gmail.com>
2018-10-13 13:42:35 -0700

    Added initializer list constructor for matrix class.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <algorithm>
 #include <memory>
 #include <vector>
+#include <initializer_list>
 #include <sys/time.h>
+
+#define N 3000
 
 uint64_t get_time()
 {
@@ -10,29 +14,51 @@ uint64_t get_time()
   return tv.tv_sec * 1000000 + tv.tv_usec;
 }
 
-#define N 3000
-
 template <class T>
 struct mat
 {
   mat(size_t nrow, size_t ncol) : nrow_(nrow), ncol_(ncol)
   {
-    vv = std::vector<std::vector<T>>(nrow_, std::vector<T>(ncol_, 0));
+    vv_ = std::vector<std::vector<T>>(nrow_, std::vector<T>(ncol_, 0));
   }
 
-  std::vector<T>& operator[] (size_t i)
+  mat(std::initializer_list<std::initializer_list<T>> lists)
   {
-    return vv[i];
+    for (const auto& l : lists)
+      vv_.push_back(l);
+    
+    nrow_ = vv_.size();
+    ncol_ = vv_.at(0).size();
+  }
+
+  mat(const mat &m)
+  {
+    vv_ = m.vv_;
+    nrow_ = m.nrow_;
+    ncol_ = m.ncol_;
+  }
+
+  mat& operator=(mat m)
+  {
+    std::swap(vv_, m.vv_);
+    std::swap(nrow_, m.nrow_);
+    std::swap(ncol_, m.ncol_);
+    return *this;
+  }
+
+  std::vector<T>& operator[](size_t i)
+  {
+    return vv_[i];
   }
 
   size_t nrow() { return nrow_; }
   size_t ncol() { return ncol_; }
-  
-  using matrix_t = std::vector<std::vector<T>>;
-  typename matrix_t::iterator begin() { return vv.begin(); }
-  typename matrix_t::iterator end()   { return vv.end();   }
-private:
-  matrix_t vv;
+
+  using matrix_t = std::vector<std::vector<T>>;  
+  typename matrix_t::iterator begin() { return vv_.begin(); }
+  typename matrix_t::iterator end()   { return vv_.end();   }
+protected:
+  std::vector<std::vector<T>> vv_;
   size_t nrow_;
   size_t ncol_;
 };
@@ -79,7 +105,7 @@ mat<T> mmult_bad(mat<T> a, mat<T> b)
 }
 
 template <class T>
-void print_mat(mat<T> &m)
+void print_mat(mat<T> m)
 {
   for (const auto& row : m)
   {
@@ -93,5 +119,11 @@ int main(int argc, char * argv[])
 {
   mat<double> m(N,N);
   matrix_initialize_test(m, m);
+
+  mat<int> a = {{1, 2}, {3, 4}};
+  mat<int> b = a;
+  print_mat(a);
+  print_mat(b);
+  print_mat(mmult_bad(a, b));
   return 0;
 }
